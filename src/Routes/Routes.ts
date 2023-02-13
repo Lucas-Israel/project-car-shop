c2801cbe8ec57d11af04ad6e45fb8345655c282c
Lucas Israel <lisraelalves92@gmail.com>
2023-02-13 17:31:44 -0300

    added routes for car

---
+++ src/Routes/Routes.ts
@@ -0,0 +1,13 @@
+// src/Routes/Routes.ts
+
+import { Router } from 'express';
+import CarController from '../Controllers/Car.controller';
+
+const routes = Router();
+
+routes.post(
+  '/cars',
+  (req, res, next) => new CarController(req, res, next).create(),
+);
+
+export default routes;
