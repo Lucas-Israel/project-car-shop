23ad77b34161dc1054337923ab3d671088f4b98e
Lucas Israel <lisraelalves92@gmail.com>
2023-02-13 19:27:12 -0300

    improved tests

--- tests/unit/Services/createCar.test.ts
+++ tests/unit/Services/createCar.test.ts
@@ -5,8 +5,8 @@ import ICar from '../../../src/Interfaces/ICar';
 import Car from '../../../src/Domains/Car';
 import CarService from '../../../src/Services/Car.service';
 
-describe('Testando camada service da rota cars', function () {
-  it('01 - Cria um carro com sucesso', async function () {
+describe('Unit tests for car.service', function () {
+  it('01 - Creates a new car with the method createCar', async function () {
     const carToSend: ICar = {
       model: 'Mareass',
       year: 2002,
@@ -38,7 +38,7 @@ describe('Testando camada service da rota cars', function () {
     sinon.restore();
   });
 
-  it('02 - Lista todos os carros com sucesso', async function () {
+  it('02 - Lists all cars with the method findAll', async function () {
     const carResolution = [{
       id: '63eaa703ae576a7db2c1631d',
       model: 'Mareass',
@@ -65,6 +65,28 @@ describe('Testando camada service da rota cars', function () {
     const service = new CarService();
 
     const result = await service.findAll();
+
+    expect(result).to.be.deep.equal(carResolution);
+  });
+
+  it('03 - Lists one car with the method findById', async function () {
+    const carResolution = {
+      _id: '63eab0d5433c7012ae17b278',
+      model: 'Mareass',
+      year: 2002,
+      color: 'Black',
+      status: false,
+      buyValue: 15.99,
+      doorsQty: 4,
+      seatsQty: 5,
+      __v: 0,
+    };
+
+    sinon.stub(Model, 'findById').resolves(carResolution);
+
+    const service = new CarService();
+
+    const result = await service.findById(carResolution._id);
 
     expect(result).to.be.deep.equal(carResolution);
   });
