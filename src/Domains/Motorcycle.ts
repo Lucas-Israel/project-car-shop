6d14924b062d2bdaea2bde2cf8eff856793dd9f2
Lucas Israel <lisraelalves92@gmail.com>
2023-02-14 12:48:44 -0300

    motorcycle domain

---
+++ src/Domains/Motorcycle.ts
@@ -0,0 +1,44 @@
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import VehicleDomain from './VehicleDomain';
+
+export default class MotorcycleDomain extends VehicleDomain {
+  private category: string;
+  private engineCapacity: number;
+  constructor({ 
+    category, 
+    engineCapacity,
+    id,
+    model,
+    year,
+    color,
+    status,
+    buyValue,
+  }: IMotorcycle) {
+    super(
+      id,
+      model,
+      year,
+      color,
+      status,
+      buyValue,
+    );
+    this.category = category;
+    this.engineCapacity = engineCapacity;
+  }
+
+  public setcategory(category: string) {
+    this.category = category;
+  }
+
+  public getcategory() {
+    return this.category;
+  }
+
+  public setengineCapacity(engineCapacity: number) {
+    this.engineCapacity = engineCapacity;
+  }
+
+  public getengineCapacity() {
+    return this.engineCapacity;
+  }
+}
