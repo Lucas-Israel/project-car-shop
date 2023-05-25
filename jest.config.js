ef1f5a30884a9ef335d14c19bacaa7490bc73e40
Lucas Israel <lisraelalves92@gmail.com>
2023-05-25 15:18:17 -0300

    fix: fixing test.config and ports for docker-compose.yml

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  rootDir: './__tests__',
-  setupFilesAfterEnv: ['./setup.js'],
-  testSequencer: './testSequencer.js',
-  modulePathIgnorePatterns: [
-    '<rootDir>/utils',
-    '<rootDir>/sources',
-    '<rootDir>/setup.js',
-    '<rootDir>/testSequencer.js',
-  ],
+  // rootDir: './__tests__',
+  // setupFilesAfterEnv: ['./setup.js'],
+  // testSequencer: './testSequencer.js',
+  // modulePathIgnorePatterns: [
+  //   '<rootDir>/utils',
+  //   '<rootDir>/sources',
+  //   '<rootDir>/setup.js',
+  //   '<rootDir>/testSequencer.js',
+  // ],
   testTimeout: 60000,
 };
